428b34c901b3aa728f25a77edb3dce505e937646
fenyx82 <bookzealot82@gmail.com>
2017-03-31 21:32:38 -0500

    CRUD functions

--- SSDAO/src/main/java/com/tc/ssdao/JDBCstudent.java
+++ SSDAO/src/main/java/com/tc/ssdao/JDBCstudent.java
@@ -6,30 +6,34 @@
 package com.tc.ssdao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.*;
 
 /**
  *
  * @author w208033650
  */
-public class JDBCstudent {
-    
-private JdbcTemplate jdbcTemplate;  
-  
-public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {  
-    this.jdbcTemplate = jdbcTemplate;  
-}  
-  
-public int saveStudent(SSStudent s){  
-    String query="insert into student values('"+s.getfName()+"','"+s.getlName()+"','"+s.getdOb()+"')";  
-    return jdbcTemplate.update(query);  
-}  
-public int updateStudent(SSStudent s){  
-    String query="update student set firstname='"+s.getfName()+"',lastname='"+s.getlName()+"' DOB='"+s.getdOb()+"' ";  
-    return jdbcTemplate.update(query);  
-}  
-public int deleteStudent(SSStudent s){  
-    String query="delete from student where id='"+s.getlName()+"' ";  
-    return jdbcTemplate.update(query);  
-}  
-  
-}  
+public class JDBCstudent extends SSStudent {
+
+    private String updateSql;
+    private JdbcTemplate jdbcTemplate;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int saveStudent(SSStudent s) {
+        String query = "insert into student values('" + s.getfName() + "','" + s.getlName() + "','" + s.getdOb() + "')";
+        return jdbcTemplate.update(query);
+    }
+
+    public void update(SSStudent s) {
+        updateSql = "update student set firstname=?,lastname=? where dob=?";
+        jdbcTemplate.update(updateSql, new Object[]{s.getfName(), s.getlName(), s.getdOb()});
+    }
+
+    public int deleteStudent(SSStudent s) {
+        String query = "delete from student where lastname='" + s.getlName() + "' ";
+        return jdbcTemplate.update(query);
+    }
+
+}
--- SSDAO/src/main/java/com/tc/ssdao/JDBCstudenttest.java
+++ SSDAO/src/main/java/com/tc/ssdao/JDBCstudenttest.java
@@ -12,22 +12,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  *
  * @author w208033650
  */
-public class JDBCstudenttest {
+public class JDBCstudenttest extends JDBCstudent {
 
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContextDAO.xml");
 
         JDBCstudent sdb = (JDBCstudent) ctx.getBean("sdao");
-        int status = sdb.saveStudent(new SSStudent("John", "Smith", "2/2/1991"));
+        /*int status = sdb.saveStudent(new SSStudent("John", "Smith", "1/1/1981"));
         System.out.println(status);
 
-        /*int status=dao.updateEmployee(new Employee(102,"Sonoo",15000)); 
-    System.out.println(status); 
-         */
- /*Employee e=new Employee(); 
-    e.setId(102); 
-    int status=dao.deleteEmployee(e); 
-    System.out.println(status);*/
+        
+        sdb.update(new SSStudent("Jackass", "Jones", "2/2/1991"));*/
+
+        SSStudent s = new SSStudent();
+        s.setlName("Smith");
+        int status = sdb.deleteStudent(s);
+        System.out.println(status);  
     }
 
 }
